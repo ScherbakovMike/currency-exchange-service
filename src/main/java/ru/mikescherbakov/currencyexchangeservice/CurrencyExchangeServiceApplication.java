84182f2f3a670c98bf9d4801ea83423ac3b17f9b
mike <scherbakov.mike@gmail.com>
2022-08-19 15:51:50 +0300

    implemented Eureka Discovery Server

--- src/main/java/ru/mikescherbakov/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/ru/mikescherbakov/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,9 +2,10 @@ package ru.mikescherbakov.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-        //(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
     public static void main(String[] args) {
